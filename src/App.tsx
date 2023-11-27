122a2373b269fec75b0b082f7f9b23bf17cc0bf7
Franco <francoazari@gmail.com>
2023-11-27 08:16:52 -0300

    Fix navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-import { Navbar } from "./components/Navbar";
+import { NavbarComponent } from "./components/NavbarComponent";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Navbar />
+    <NavbarComponent />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
